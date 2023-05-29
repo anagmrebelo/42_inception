c551ec668a1b6dfb811a038db03332f0a177be88
arebelo <anagmrebelo@hotmail.com>
2023-05-29 17:28:24 +0100

    uncommented mariadb scriot

--- srcs/requirements/mariadb/tools/mariadb_start.sh
+++ srcs/requirements/mariadb/tools/mariadb_start.sh
@@ -5,6 +5,6 @@ mysql -u root -e "CREATE DATABASE $DB_NAME;"
 mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
 
-# mysqld
+mysqld
 
 # If container restarts database is created again?
