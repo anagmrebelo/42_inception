9eb84fed25531cd5d67b3bff77dae313b23de212
arebelo <anagmrebelo@hotmail.com>
2023-05-30 15:54:10 +0100

    one line

--- srcs/requirements/mariadb/tools/mariadb_start.sh
+++ srcs/requirements/mariadb/tools/mariadb_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 service mysql start
-# mysql -u root -e "CREATE DATABASE $DB_NAME;"
+mysql -u root -e "CREATE DATABASE $DB_NAME;"
 # mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
 # #mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
 # mysql -u root -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' WITH GRANT OPTION;"
