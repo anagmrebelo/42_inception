54e4f5502b0c5a523f4b0acff9fb0a24c852f0a5
arebelo <anagmrebelo@hotmail.com>
2023-05-30 12:45:56 +0100

    args in docker compose

--- srcs/requirements/mariadb/tools/create_user.sh
+++ srcs/requirements/mariadb/tools/create_user.sh
@@ -1,7 +1,3 @@
-# userdel mysql
-# useradd -u 999 mysql
-# chown -R mysql:mysql /var/lib/mysql
-
 service mysql start
 echo $MYSQL_DATABASE > log.txt
 mysql -u root -e "CREATE DATABASE $MYSQL_DATABASE;"
