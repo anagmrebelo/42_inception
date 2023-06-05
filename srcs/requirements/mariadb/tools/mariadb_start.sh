1ae68b622d6af5be7ef6bd4aa1e3a363f360135d
arebelo <anagmrebelo@hotmail.com>
2023-06-05 11:51:20 +0100

    Granted privelleges to DB_USER

--- srcs/requirements/mariadb/tools/mariadb_start.sh
+++ srcs/requirements/mariadb/tools/mariadb_start.sh
@@ -6,4 +6,6 @@ if [ ! -d /var/lib/mysql/$DB_NAME ];
 then
 	mysql -u root -e "CREATE DATABASE $DB_NAME;"
 	mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+	mysql -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' WITH GRANT OPTION;"
+	mysql -u root -e "FLUSH PRIVILEGES;"
 fi
