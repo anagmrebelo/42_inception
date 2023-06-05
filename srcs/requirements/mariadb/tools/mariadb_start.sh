3ce9dc60e1083637fca40017211e667d97dfd70a
arebelo <anagmrebelo@hotmail.com>
2023-06-05 11:35:33 +0100

    add new user

--- srcs/requirements/mariadb/tools/mariadb_start.sh
+++ srcs/requirements/mariadb/tools/mariadb_start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 service mysql start
-mysql -u root -e "CREATE DATABASE $DB_NAME;"
+
+if [ ! -d /var/lib/mysql/$DB_NAME ];
+then
+	mysql -u root -e "CREATE DATABASE $DB_NAME;"
+	mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+fi
