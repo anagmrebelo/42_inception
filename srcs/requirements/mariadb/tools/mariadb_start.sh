67c8e3f2b5c0297ecb1c740e6dc704fd369c7739
arebelo <anagmrebelo@hotmail.com>
2023-06-05 12:02:29 +0100

    Added root password

--- srcs/requirements/mariadb/tools/mariadb_start.sh
+++ srcs/requirements/mariadb/tools/mariadb_start.sh
@@ -8,4 +8,5 @@ then
 	mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
 	mysql -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' WITH GRANT OPTION;"
 	mysql -u root -e "FLUSH PRIVILEGES;"
+	mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD'";
 fi
