624cc8ad884d50302660358b6116e573c973b2fd
Ana Rebelo <arebelo@student.42barcelona.com>
2023-11-22 07:32:19 -0500

    Update WP admin password and remove unused files

--- srcs/requirements/wordpress/tools/wordpress_start.sh
+++ srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -5,7 +5,8 @@ then
 	cd /var/www/html
 	wp core download --allow-root
 	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --allow-root
-	wp core install --url=$WP_DOMAIN --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWPRD --admin_email=$WP_EMAIL --allow-root
+	wp core install --url=$WP_DOMAIN --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_EMAIL --allow-root
+	wp user create $WP_USER_NAME $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=editor --allow-root
 fi
 
 /usr/sbin/php-fpm7.4 -F
